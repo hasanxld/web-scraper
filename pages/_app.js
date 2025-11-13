8b2e706eac600d5852e729de13aca02a02e3ba5d
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 22:23:31 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/globals.css'
+import LoadingBar from '../components/LoadingBar'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    // Auto-detect and set site URL
+    if (typeof window !== 'undefined') {
+      const siteUrl = window.location.origin
+      if (!process.env.NEXT_PUBLIC_SITE_URL) {
+        process.env.NEXT_PUBLIC_SITE_URL = siteUrl
+      }
+    }
+  }, [])
+
+  return (
+    <>
+      <LoadingBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
