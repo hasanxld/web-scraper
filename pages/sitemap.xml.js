61d0c9d50fd5c57757be4e291a9d0fe2e27d32ea
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 22:31:54 +0600

    Create sitemap.xml.js

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,38 @@
+const Sitemap = () => {}
+
+export async function getServerSideProps({ res }) {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://your-domain.vercel.app'
+  
+  const staticPages = [
+    '',
+    '/tool',
+    '/docs',
+    '/dashboard',
+    '/generate',
+    '/privacy',
+    '/terms',
+    '/contact'
+  ]
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages.map((page) => `
+    <url>
+      <loc>${baseUrl}${page}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>${page === '' ? '1.0' : '0.8'}</priority>
+    </url>
+  `).join('')}
+</urlset>`
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default Sitemap
