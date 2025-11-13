ae36be8429f048b9f89aa0dab52589be2520adc0
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 21:37:10 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, x-api-key' },
+        ],
+      },
+    ]
+  },
+  env: {
+    SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  }
+}
+
+module.exports = nextConfig
